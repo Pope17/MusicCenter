d6f745efb5696d74824b01685727364b33fdb45f
Pope17 <ayrtonjuarez97@gmail.com>
2021-10-18 17:10:03 -0300

    Realicé el product Detail, agregué el header y el footer del home

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const PUERTO = 3000;
 app.use(express.static('./public'));
 
 app.listen(PUERTO, function(){
-    console.log('Servidor corriendo')
+    console.log('Servidor corriendo en la ruta 3000')
 });
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, './views/home.html'))
+})
+
+app.get('/productDetail', function(req, res){
+    res.sendFile(path.join(__dirname, './views/productDetail.html'))
 })
