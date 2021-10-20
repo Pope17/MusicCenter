4d3bc72730cffae65ea4731b3b1e3936629c5f8d
ManuelRatto <manuelrattojorgensen@gmail.com>
2021-10-20 20:24:53 -0300

    hice el carrito

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PUERTO = 3000;
+
+app.use(express.static('./public'));
+
+
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
+
+app.get('/productDetail', function(req, res){
+    res.sendFile(path.join(__dirname, './views/productDetail.html'))
+})
+app.get('/register', function(req,res){
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
+
+app.get('/login', function(req,res){
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
+
+app.get('/productCart', function(req,res){
+    res.sendFile(path.join(__dirname, './views/productCart.html'))
+})
+
+app.listen(PUERTO, function(){
+    console.log('Servidor corriendo en la ruta 3000')
+});
