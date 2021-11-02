73a933b30b7098da1be8c88dccfe310389e9d205
Ramiro Beccar Varela <djchimango@gmail.com>
2021-11-02 16:53:06 -0300

    refaccion de ejs y algunas rutas

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,13 +1,30 @@
-const path = require('path');
 
 const mainController = {
     index: (req, res) => {  
-    res.sendFile(path.join(__dirname, '../views/home.html')) 
+    res.render('home') 
 },
 
+    login: (req, res) => {  
+ //* const product = producto.find(req.params.id )*// 
+        res.render('login')
+},       
+
+register: (req, res) => {  
+    res.render('register')
+},       
 
 about: (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/about.html'))
-    }
+    res.render(path.join(__dirname, '../views/about.html'))
+},
+
+productCart: (req, res) => {  
+    res.render('productCart')
+},
+
+productDetail: (req, res) => {  
+    res.render('productDeatil')
+},       
+
 }
+
 module.exports = mainController;
